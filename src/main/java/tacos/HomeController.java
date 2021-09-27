1751dcf93b754315158b8cdfac3c90679848d8ca
PLara-r <tl1226932@gmail.com>
2021-09-27 16:43:46 +0300

    First commit

---
+++ src/main/java/tacos/HomeController.java
@@ -0,0 +1,12 @@
+package tacos;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+@Controller                 /*Контроллер*/
+public class HomeController {
+
+ @GetMapping("/")          /*Обрабатывает запросы корневого пути */
+ public String home() {
+   return "home"; /*Возвращает имя представления*/
+   }
+}
---
+++ src/test/java/tacos/TacoCloudApplicationTests.java
@@ -0,0 +1,18 @@
+
+
+
+
+
+package tacos;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+      @RunWith(SpringRunner.class)  /*Использование Spring runner*/
+@SpringBootTest /* String Boot тест*/
+public class TacoCloudApplicationTests {
+   @Test /*Метод теста*/
+   public void contextLoads() {
+   }
+}
